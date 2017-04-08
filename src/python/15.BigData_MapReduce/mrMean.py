6bdfd5a42072ecac8fb2fff6fea45323b1dad1e5
jiangzhonglian <jiang-s@163.com>
2017-04-08 18:06:58 +0800

    更新15.mapreduce的使用注解

--- src/python/15.BigData_MapReduce/mrMean.py
+++ src/python/15.BigData_MapReduce/mrMean.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# coding:utf8
+
 '''
 Created on 2017-04-07
 
@@ -5,26 +8,27 @@ Created on 2017-04-07
 '''
 from mrjob.job import MRJob
 
+
 class MRmean(MRJob):
     def __init__(self, *args, **kwargs): # 对数据初始化
         super(MRmean, self).__init__(*args, **kwargs)
         self.inCount = 0
         self.inSum = 0
         self.inSqSum = 0
-    
+
     def map(self, key, val): # 需要 2 个参数，求数据的和与平方和
         if False: yield
         inVal = float(val)
         self.inCount += 1
         self.inSum += inVal
         self.inSqSum += inVal*inVal
-        
+
     def map_final(self): # 计算数据的平均值，平方的均值，并返回
         mn = self.inSum/self.inCount
         mnSq = self.inSqSum/self.inCount
         yield (1, [self.inCount, mn, mnSq])
 
-    def reduce(self, key, packedValues): # 
+    def reduce(self, key, packedValues):
         cumVal=0.0; cumSumSq=0.0; cumN=0.0
         for valArr in packedValues: # 从输入流中获取值
             nj = float(valArr[0])
@@ -34,10 +38,10 @@ class MRmean(MRJob):
         mean = cumVal/cumN
         var = (cumSumSq - 2*mean*cumVal + cumN*mean*mean)/cumN
         yield (mean, var) # 发出平均值和方差
-        
+
     def steps(self):
-        return ([self.mr(mapper=self.map, mapper_final=self.map_final,\
-                          reducer=self.reduce,)])
+        return ([self.mr(mapper=self.map, mapper_final=self.map_final, reducer=self.reduce,)])
+
 
 if __name__ == '__main__':
     MRmean.run()
