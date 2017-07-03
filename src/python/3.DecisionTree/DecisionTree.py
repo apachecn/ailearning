ce511ef9b54d2668bbf7ff2ab9496bebc2444e36
chenyyx <chinachenyyx@163.com>
2017-07-03 21:26:33 +0800

    修改图片和logistic回归的md文件

--- src/python/3.DecisionTree/DecisionTree.py
+++ src/python/3.DecisionTree/DecisionTree.py
@@ -38,7 +38,7 @@ def createDataSet():
 
 
 def calcShannonEnt(dataSet):
-    """calcShannonEnt(calculate Shannon entropy 计算label分类标签的香农熵)
+    """calcShannonEnt(calculate Shannon entropy 计算给定数据集的香农熵)
 
     Args:
         dataSet 数据集
@@ -47,13 +47,17 @@ def calcShannonEnt(dataSet):
     """
     # 求list的长度，表示计算参与训练的数据量
     numEntries = len(dataSet)
+    # 下面输出我们测试的数据集的一些信息
+    # 例如：<type 'list'> numEntries:  5 是下面的代码的输出
     # print type(dataSet), 'numEntries: ', numEntries
 
     # 计算分类标签label出现的次数
     labelCounts = {}
     # the the number of unique elements and their occurance
     for featVec in dataSet:
+        # 将当前实例的标签存储，即每一行数据的最后一个数据代表的是标签
         currentLabel = featVec[-1]
+        # 为所有可能的分类创建字典，如果当前的键值不存在，则扩展字典并将当前键值加入字典。每个键值都记录了当前类别出现的次数。
         if currentLabel not in labelCounts.keys():
             labelCounts[currentLabel] = 0
         labelCounts[currentLabel] += 1
@@ -62,8 +66,10 @@ def calcShannonEnt(dataSet):
     # 对于label标签的占比，求出label标签的香农熵
     shannonEnt = 0.0
     for key in labelCounts:
+        # 使用所有类标签的发生频率计算类别出现的概率。
         prob = float(labelCounts[key])/numEntries
         # log base 2
+        # 计算香农熵，以 2 为底求对数
         shannonEnt -= prob * log(prob, 2)
         # print '---', prob, prob * log(prob, 2), shannonEnt
     return shannonEnt
@@ -71,24 +77,42 @@ def calcShannonEnt(dataSet):
 
 def splitDataSet(dataSet, axis, value):
     """splitDataSet(通过遍历dataSet数据集，求出axis对应的colnum列的值为value的行)
-
+        就是依据axis列进行分类，如果axis列的数据等于 value的时候，就要将 axis 划分到我们创建的新的数据集中
     Args:
-        dataSet 数据集
-        axis 表示每一行的axis列
-        value 表示axis列对应的value值
+        dataSet 数据集                 待划分的数据集
+        axis 表示每一行的axis列         划分数据集的特征
+        value 表示axis列对应的value值   需要返回的特征的值。
     Returns:
         axis列为value的数据集【该数据集需要排除axis列】
     """
     retDataSet = []
-    for featVec in dataSet:
+    for featVec in dataSet: 
         # axis列为value的数据集【该数据集需要排除axis列】
+        # 判断axis列的值是否为value
         if featVec[axis] == value:
             # chop out axis used for splitting
+            # [:axis]表示前axis行，即若 axis 为2，就是取 featVec 的前 axis 行
             reducedFeatVec = featVec[:axis]
             '''
             请百度查询一下： extend和append的区别
+            list.append(object) 向列表中添加一个对象object
+            list.extend(sequence) 把一个序列seq的内容添加到列表中
+            1、使用append的时候，是将new_media看作一个对象，整体打包添加到music_media对象中。
+            2、使用extend的时候，是将new_media看作一个序列，将这个序列和music_media序列合并，并放在其后面。
+            result = []
+            result.extend([1,2,3])
+            print result
+            result.append([4,5,6])
+            print result
+            result.extend([7,8,9])
+            print result
+            结果：
+            [1, 2, 3]
+            [1, 2, 3, [4, 5, 6]]
+            [1, 2, 3, [4, 5, 6], 7, 8, 9
             '''
             reducedFeatVec.extend(featVec[axis+1:])
+            # [axis+1:]表示从跳过 axis 的 axis+1行，取接下来的数据
             # 收集结果值 axis列为value的行【该行需要排除axis列】
             retDataSet.append(reducedFeatVec)
     return retDataSet
@@ -111,19 +135,21 @@ def chooseBestFeatureToSplit(dataSet):
     # iterate over all the features
     for i in range(numFeatures):
         # create a list of all the examples of this feature
-        # 获取每一个feature的list集合
+        # 获取每一个实例的第i+1个feature，组成list集合
         featList = [example[i] for example in dataSet]
         # get a set of unique values
-        # 获取剔重后的集合
+        # 获取剔重后的集合，使用set对list数据进行去重
         uniqueVals = set(featList)
         # 创建一个临时的信息熵
         newEntropy = 0.0
-        # 遍历某一列的value集合，计算该列的信息熵
+        # 遍历某一列的value集合，计算该列的信息熵 
+        # 遍历当前特征中的所有唯一属性值，对每个唯一属性值划分一次数据集，计算数据集的新熵值，并对所有唯一特征值得到的熵求和。
         for value in uniqueVals:
             subDataSet = splitDataSet(dataSet, i, value)
             prob = len(subDataSet)/float(len(dataSet))
             newEntropy += prob * calcShannonEnt(subDataSet)
         # gain[信息增益]: 划分数据集前后的信息变化， 获取信息熵最大的值
+        # 信息增益是熵的减少或者是数据无序度的减少。最后，比较所有特征中的信息增益，返回最好特征划分的索引值。
         infoGain = baseEntropy - newEntropy
         print 'infoGain=', infoGain, 'bestFeature=', i, baseEntropy, newEntropy
         if (infoGain > bestInfoGain):
@@ -145,7 +171,7 @@ def majorityCnt(classList):
         if vote not in classCount.keys():
             classCount[vote] = 0
         classCount[vote] += 1
-    # 倒叙排列classCount得到一个字典集合，然后取出第一个就是结果（yes/no）
+    # 倒叙排列classCount得到一个字典集合，然后取出第一个就是结果（yes/no），即出现次数最多的结果
     sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
     # print 'sortedClassCount:', sortedClassCount
     return sortedClassCount[0][0]
@@ -154,13 +180,16 @@ def majorityCnt(classList):
 def createTree(dataSet, labels):
     classList = [example[-1] for example in dataSet]
     # 如果数据集的最后一列的第一个值出现的次数=整个集合的数量，也就说只有一个类别，就只直接返回结果就行
+    # 第一个停止条件：所有的类标签完全相同，则直接返回该类标签。
+    # count() 函数是统计括号中的值在list中出现的次数
     if classList.count(classList[0]) == len(classList):
         return classList[0]
     # 如果数据集只有1列，那么最初出现label次数最多的一类，作为结果
+    # 第二个停止条件：使用完了所有特征，仍然不能将数据集划分成仅包含唯一类别的分组。
     if len(dataSet[0]) == 1:
         return majorityCnt(classList)
 
-    # 选择最优的列，得到最有列对应的label含义
+    # 选择最优的列，得到最优列对应的label含义
     bestFeat = chooseBestFeatureToSplit(dataSet)
     # 获取label的名称
     bestFeatLabel = labels[bestFeat]
@@ -175,6 +204,7 @@ def createTree(dataSet, labels):
     for value in uniqueVals:
         # 求出剩余的标签label
         subLabels = labels[:]
+        # 遍历当前选择特征包含的所有属性值，在每个数据集划分上递归调用函数createTree()
         myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value), subLabels)
         # print 'myTree', value, myTree
     return myTree
