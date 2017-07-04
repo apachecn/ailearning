8de61578d93497e7c52267422232ece05471fe60
chenyyx <chinachenyyx@163.com>
2017-07-04 19:59:10 +0800

    添加决策树，朴素贝叶斯和回归的sklearn版本，logistic回归的sklearn版本

--- src/python/4.NaiveBayes/sklearn-nb-demo.py
+++ src/python/4.NaiveBayes/sklearn-nb-demo.py
@@ -8,73 +8,38 @@ NaiveBayes：朴素贝叶斯
 @author: 小瑶
 《机器学习实战》更新地址：https://github.com/apachecn/MachineLearning
 """
+
+
+# GaussianNB_高斯朴素贝叶斯
 import numpy as np
-import matplotlib.pyplot as plt
-from sklearn import svm
-print(__doc__)
-
-
-# 创建40个分离点
-np.random.seed(0)
-# X = np.r_[np.random.randn(20, 2) - [2, 2], np.random.randn(20, 2) + [2, 2]]
-# Y = [0] * 20 + [1] * 20
-
-
-def loadDataSet(fileName):
-    """
-    对文件进行逐行解析，从而得到第行的类标签和整个数据矩阵
-    Args:
-        fileName 文件名
-    Returns:
-        dataMat  数据矩阵
-        labelMat 类标签
-    """
-    dataMat = []
-    labelMat = []
-    fr = open(fileName)
-    for line in fr.readlines():
-        lineArr = line.strip().split('\t')
-        dataMat.append([float(lineArr[0]), float(lineArr[1])])
-        labelMat.append(float(lineArr[2]))
-    return dataMat, labelMat
-
-
-X, Y = loadDataSet('input/6.SVM/testSet.txt')
-X = np.mat(X)
-
-print("X=", X)
-print("Y=", Y)
-
-# 拟合一个SVM模型
-clf = svm.SVC(kernel='linear')
+X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
+Y = np.array([1, 1, 1, 2, 2, 2])
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
 clf.fit(X, Y)
+print clf.predict([[-0.8, -1]])
+clf_pf = GaussianNB()
+clf_pf.partial_fit(X, Y, np.unique(Y))
+print clf_pf.predict([[-0.8, -1]])
 
-# 获取分割超平面
-w = clf.coef_[0]
-# 斜率
-a = -w[0] / w[1]
-# 从-5到5，顺序间隔采样50个样本，默认是num=50
-# xx = np.linspace(-5, 5)  # , num=50)
-xx = np.linspace(-2, 10)  # , num=50)
-# 二维的直线方程
-yy = a * xx - (clf.intercept_[0]) / w[1]
-print("yy=", yy)
+# MultinomialNB_多项朴素贝叶斯
+'''
+import numpy as np
+X = np.random.randint(5, size=(6, 100))
+y = np.array([1, 2, 3, 4, 5, 6])
+from sklearn.naive_bayes import MultinomialNB
+clf = MultinomialNB()
+clf.fit(X, y)
+print clf.predict(X[2:3])
+'''
 
-# plot the parallels to the separating hyperplane that pass through the support vectors
-# 通过支持向量绘制分割超平面
-print("support_vectors_=", clf.support_vectors_)
-b = clf.support_vectors_[0]
-yy_down = a * xx + (b[1] - a * b[0])
-b = clf.support_vectors_[-1]
-yy_up = a * xx + (b[1] - a * b[0])
-
-# plot the line, the points, and the nearest vectors to the plane
-plt.plot(xx, yy, 'k-')
-plt.plot(xx, yy_down, 'k--')
-plt.plot(xx, yy_up, 'k--')
-
-plt.scatter(clf.support_vectors_[:, 0], clf.support_vectors_[:, 1], s=80, facecolors='none')
-plt.scatter(X[:, 0], X[:, 1], c=Y, cmap=plt.cm.Paired)
-
-plt.axis('tight')
-plt.show()
+# BernoulliNB_伯努利朴素贝叶斯
+'''
+import numpy as np
+X = np.random.randint(2, size=(6, 100))
+Y = np.array([1, 2, 3, 4, 4, 5])
+from sklearn.naive_bayes import BernoulliNB
+clf = BernoulliNB()
+clf.fit(X, Y)
+print clf.predict(X[2:3])
+'''
--- src/python/8.Predictive numerical data regression/regression.py
+++ src/python/8.Predictive numerical data regression/regression.py
@@ -4,7 +4,7 @@
 '''
 Created on Jan 8, 2011
 Update  on 2017-05-18
-@author: Peter Harrington/ApacheCN-小瑶
+@author: Peter Harrington/小瑶
 《机器学习实战》更新地址：https://github.com/apachecn/MachineLearning
 '''
 
@@ -12,37 +12,74 @@ Update  on 2017-05-18
 from numpy import *
 import matplotlib.pylab as plt
 
-def loadDataSet(fileName):                 # 解析以tab键分隔的文件中的浮点数
+def loadDataSet(fileName):                 
     """ 加载数据
+        解析以tab键分隔的文件中的浮点数
     Returns：
-        dataMat  feature 对应的数据集
-        labelMat feature 对应的分类标签，即类别标签
+        dataMat ：  feature 对应的数据集
+        labelMat ： feature 对应的分类标签，即类别标签
 
     """
-    numFeat = len(open(fileName).readline().split('\t')) - 1 # 获得每一行的输入数据，最后一个代表真实值 
-    dataMat = []; labelMat = []
+    # 获取样本特征的总数，不算最后的目标变量 
+    numFeat = len(open(fileName).readline().split('\t')) - 1 
+    dataMat = []
+    labelMat = []
     fr = open(fileName)
-    for line in fr.readlines():            # 读取每一行
+    for line in fr.readlines():
+        # 读取每一行
         lineArr =[]
-        curLine = line.strip().split('\t') # 删除一行中以tab分隔的数据前后的空白符号
-        for i in range(numFeat):           # 从0到2，不包括2
-            lineArr.append(float(curLine[i]))# 将数据添加到lineArr List中，每一行数据测试数据组成一个行向量
-        dataMat.append(lineArr)            # 将测试数据的输入数据部分存储到dataMat矩阵中
-        labelMat.append(float(curLine[-1]))# 将每一行的最后一个数据，即真实的目标变量存储到labelMat矩阵中
+        # 删除一行中以tab分隔的数据前后的空白符号
+        curLine = line.strip().split('\t')
+        # i 从0到2，不包括2 
+        for i in range(numFeat):
+            # 将数据添加到lineArr List中，每一行数据测试数据组成一个行向量           
+            lineArr.append(float(curLine[i]))
+            # 将测试数据的输入数据部分存储到dataMat 的List中
+        dataMat.append(lineArr)
+        # 将每一行的最后一个数据，即类别，或者叫目标变量存储到labelMat List中
+        labelMat.append(float(curLine[-1]))
     return dataMat,labelMat
 
-def standRegres(xArr,yArr):               # 线性回归
-    xMat = mat(xArr); yMat = mat(yArr).T  # mat()函数将xArr，yArr转换为矩阵
-    xTx = xMat.T*xMat                     # 矩阵乘法的条件是左矩阵的列数等于右矩阵的行数
-    if linalg.det(xTx) == 0.0:            # 因为要用到xTx的逆矩阵，所以事先需要确定计算得到的xTx是否可逆，条件是矩阵的行列式不为0
-        print ("This matrix is singular, cannot do inverse")
+def standRegres(xArr,yArr):
+    '''
+    Description：
+        线性回归
+    Args:
+        xArr ：输入的样本数据，包含每个样本数据的 feature
+        yArr ：对应于输入数据的类别标签，也就是每个样本对应的目标变量
+    Returns:
+
+    '''
+
+    # mat()函数将xArr，yArr转换为矩阵 mat().T 代表的是对矩阵进行转置操作
+    xMat = mat(xArr)
+    yMat = mat(yArr).T
+    # 矩阵乘法的条件是左矩阵的列数等于右矩阵的行数
+    xTx = xMat.T*xMat
+    # 因为要用到xTx的逆矩阵，所以事先需要确定计算得到的xTx是否可逆，条件是矩阵的行列式不为0
+    # linalg.det() 函数是用来求得矩阵的行列式的，如果矩阵的行列式为0，则这个矩阵是不可逆的，就无法进行接下来的运算                   
+    if linalg.det(xTx) == 0.0:
+        print "This matrix is singular, cannot do inverse" 
         return
     # 最小二乘法
     # http://www.apache.wiki/pages/viewpage.action?pageId=5505133
-    ws = xTx.I * (xMat.T*yMat)            # 书中的公式，求得w的最优解
+    # 书中的公式，求得w的最优解
+    ws = xTx.I * (xMat.T*yMat)            
     return ws
 
-def lwlr(testPoint,xArr,yArr,k=1.0):      # 局部加权线性回归
+    # 局部加权线性回归
+def lwlr(testPoint,xArr,yArr,k=1.0):
+    '''
+        Description：
+            局部加权线性回归
+        Args：
+            testPoint：
+            xArr：
+            yArr：
+            k:
+        Returns:
+            
+    '''
     xMat = mat(xArr); yMat = mat(yArr).T
     m = shape(xMat)[0]                    # 获得xMat矩阵的行数
     weights = mat(eye((m)))               # eye()返回一个对角线元素为1，其他元素为0的二维数组，创建权重矩阵
