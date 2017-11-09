ee0525a519c6db27587e6d7d9a8c9652934b9ec0
Cugtyt <cugtyt@qq.com>
2017-11-09 19:50:36 +0800

    Simplify classify0 func in kNN
    Use list comprehension and Counter to simplify classify0 func in kNN, and tile in numpy is not as convenient as broadcasting.
    This way is more clear and neat.

--- src/python/2.KNN/kNN.py
+++ src/python/2.KNN/kNN.py
@@ -10,6 +10,7 @@ from numpy import *
 # 导入科学计算包numpy和运算符模块operator
 import operator
 from os import listdir
+from collections import Counter
 
 
 def createDataSet():
@@ -38,26 +39,10 @@ def classify0(inX, dataSet, labels, k):
     预测数据所在分类可在输入下列命令
     kNN.classify0([0,0], group, labels, 3)
     """
-    # 1. 距离计算
-    dataSetSize = dataSet.shape[0]
-    # tile生成和训练样本对应的矩阵，并与训练样本求差
+    
     """
-    tile: 列-3表示复制的行数， 行-1／2表示对inx的重复的次数
-
-    In [8]: tile(inx, (3, 1))
-    Out[8]:
-    array([[1, 2, 3],
-        [1, 2, 3],
-        [1, 2, 3]])
-
-    In [9]: tile(inx, (3, 2))
-    Out[9]:
-    array([[1, 2, 3, 1, 2, 3],
-        [1, 2, 3, 1, 2, 3],
-        [1, 2, 3, 1, 2, 3]])
-    """
-    diffMat = tile(inX, (dataSetSize, 1)) - dataSet
-    """
+    1. 计算距离
+    
     欧氏距离： 点到点之间的距离
        第一行： 同一个点 到 dataSet的第一个点的距离。
        第二行： 同一个点 到 dataSet的第二个点的距离。
@@ -66,41 +51,27 @@ def classify0(inX, dataSet, labels, k):
 
     [[1,2,3],[1,2,3]]-[[1,2,3],[1,2,0]]
     (A1-A2)^2+(B1-B2)^2+(c1-c2)^2
+    
+    inx - dataset 使用了numpy broadcasting，见 https://docs.scipy.org/doc/numpy-1.13.0/user/basics.broadcasting.html
+    np.sum() 函数的使用见 https://docs.scipy.org/doc/numpy-1.13.0/reference/generated/numpy.sum.html
     """
-    # 取平方
-    sqDiffMat = diffMat ** 2
-    # 将矩阵的每一行相加
-    sqDistances = sqDiffMat.sum(axis=1)
-    # 开方
-    distances = sqDistances ** 0.5
-    # 根据距离排序从小到大的排序，返回对应的索引位置
-    # argsort() 是将x中的元素从小到大排列，提取其对应的index（索引），然后输出到y。
-    # 例如：y=array([3,0,2,1,4,5]) 则，x[3]=-1最小，所以y[0]=3,x[5]=9最大，所以y[5]=5。
-    # print 'distances=', distances
-    sortedDistIndicies = distances.argsort()
-    # print 'distances.argsort()=', sortedDistIndicies
-
-    # 2. 选择距离最小的k个点
-    classCount = {}
-    for i in range(k):
-        # 找到该样本的类型
-        voteIlabel = labels[sortedDistIndicies[i]]
-        # 在字典中将该类型加一
-        # 字典的get方法
-        # 如：list.get(k,d) 其中 get相当于一条if...else...语句,参数k在字典中，字典将返回list[k];如果参数k不在字典中则返回参数d,如果K在字典中则返回k对应的value值
-        # l = {5:2,3:4}
-        # print l.get(3,0)返回的值是4；
-        # Print l.get（1,0）返回值是0；
-        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
-    # 3. 排序并返回出现最多的那个类型
-    # 字典的 items() 方法，以列表返回可遍历的(键，值)元组数组。
-    # 例如：dict = {'Name': 'Zara', 'Age': 7}   print "Value : %s" %  dict.items()   Value : [('Age', 7), ('Name', 'Zara')]
-    # sorted 中的第2个参数 key=operator.itemgetter(1) 这个参数的意思是先比较第几个元素
-    # 例如：a=[('b',2),('a',1),('c',0)]  b=sorted(a,key=operator.itemgetter(1)) >>>b=[('c',0),('a',1),('b',2)] 可以看到排序是按照后边的0,1,2进行排序的，而不是a,b,c
-    # b=sorted(a,key=operator.itemgetter(0)) >>>b=[('a',1),('b',2),('c',0)] 这次比较的是前边的a,b,c而不是0,1,2
-    # b=sorted(a,key=opertator.itemgetter(1,0)) >>>b=[('c',0),('a',1),('b',2)] 这个是先比较第2个元素，然后对第一个元素进行排序，形成多级排序。
-    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
-    return sortedClassCount[0][0]
+	dist = np.sum((inx - dataset)**2, axis=1)**0.5
+    
+    """
+	2. k个最近的标签
+    
+    对距离排序使用numpy中的argsort函数， 见 https://docs.scipy.org/doc/numpy-1.13.0/reference/generated/numpy.sort.html#numpy.sort
+    函数返回的是索引，因此取前k个索引使用[0 : k]
+    将这k个标签存在列表k_labels中
+    """
+    k_labels = [labels[index] for index in dist.argsort()[0 : k]]
+	"""
+    3. 出现次数最多的标签即为最终类别
+    
+    使用collections.Counter可以统计各个标签的出现次数，most_common返回出现次数最多的标签tuple，例如[('lable1', 2)]，因此[0][0]可以取出标签值
+	"""
+    label = Counter(k_labels).most_common(1)[0][0]
+    return label
 
 
 def test1():
