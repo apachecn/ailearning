2f3854bf01eed4976e2738740747e6d47ee65099
VPrincekin <dehong_w@163.com>
2017-12-14 12:43:38 +0800

    修改SVM-python3.6部分代码，测试正常运行

--- src/python/6.SVM/svm-complete_Non-Kernel.py
+++ src/python/6.SVM/svm-complete_Non-Kernel.py
@@ -110,8 +110,8 @@ def selectJ(i, oS, Ei):  # this is the second choice -heurstic, and calcs Ej
     # 首先将输入值Ei在缓存中设置成为有效的。这里的有效意味着它已经计算好了。
     oS.eCache[i] = [1, Ei]
 
-    # print 'oS.eCache[%s]=%s' % (i, oS.eCache[i])
-    # print 'oS.eCache[:, 0].A=%s' % oS.eCache[:, 0].A.T
+    # print('oS.eCache[%s]=%s' % (i, oS.eCache[i]))
+    # print('oS.eCache[:, 0].A=%s' % oS.eCache[:, 0].A.T)
     # """
     # # 返回非0的：行列值
     # nonzero(oS.eCache[:, 0].A)= (
@@ -119,9 +119,9 @@ def selectJ(i, oS, Ei):  # this is the second choice -heurstic, and calcs Ej
     #     列： array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 0, 0, 0, 0])
     # )
     # """
-    # print 'nonzero(oS.eCache[:, 0].A)=', nonzero(oS.eCache[:, 0].A)
+    # print('nonzero(oS.eCache[:, 0].A)=', nonzero(oS.eCache[:, 0].A))
     # # 取行的list
-    # print 'nonzero(oS.eCache[:, 0].A)[0]=', nonzero(oS.eCache[:, 0].A)[0]
+    # print('nonzero(oS.eCache[:, 0].A)[0]=', nonzero(oS.eCache[:, 0].A)[0])
     # 非零E值的行的list列表，所对应的alpha值
     validEcacheList = nonzero(oS.eCache[:, 0].A)[0]
     if (len(validEcacheList)) > 1:
@@ -366,7 +366,7 @@ def plotfig_SVM(xArr, yArr, ws, b, alphas):
 if __name__ == "__main__":
     # 获取特征和目标变量
     dataArr, labelArr = loadDataSet('../../../input/6.SVM/testSet.txt')
-    # print labelArr
+    # print(labelArr)
 
     # b是常量值， alphas是拉格朗日乘子
     b, alphas = smoP(dataArr, labelArr, 0.6, 0.001, 40)
