4df173179023f24b64eb5976e22a2c5f311efff6
Watermelon233 <1034616238@qq.com>
2018-03-23 19:03:34 +0800

    修改了zip没有len的问题
    由于运行时间太长，没有跑完一个epoch,不清楚网络是否学到了东西，若没学到可以根据注释查找一下问题

--- src/deeplearning/py3.x/mnist.py
+++ src/deeplearning/py3.x/mnist.py
@@ -98,14 +98,15 @@ def get_test_data_set():
     获得测试数据集
     '''
     # 源程序文件名为t10k-images-idx3-ubyte，无论是修改数据文件名或程序均可
-    image_loader = ImageLoader('t10k-images.idx3-ubyte', 10)
-    label_loader = LabelLoader('t10k-labels.idx1-ubyte', 10)
+    image_loader = ImageLoader('t10k-images.idx3-ubyte',10)
+    label_loader = LabelLoader('t10k-labels.idx1-ubyte',10)
     return image_loader.load(), label_loader.load()
 
 
 def get_result(vec):
     max_value_index = 0
     max_value = 0
+    vec = list(vec)#python3中 zip需要强制转换为list否则没有长度
     for i in range(len(vec)):
         if vec[i]>max_value:
             max_value_index = i
@@ -145,7 +146,12 @@ def train_and_evaluate():
                 last_error_ratio = error_ratio
 if __name__ == '__main__':
     train_and_evaluate()
-
+    '''
+    小批数据错误率不下降可能是因为数据输入时没有进行打乱，程序一直在学习某个数字
+    如果大批量数据，error ratio仍不下降，问题可能在于文件读取这块。
+    建议查看二进制文件读取出的content转化为list后是否正确，
+    是否能从文件中看出数字轮廓
+    '''
 '''
     import numpy as np
     import seaborn as sns
