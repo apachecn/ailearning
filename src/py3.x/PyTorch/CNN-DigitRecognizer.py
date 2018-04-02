13655441476f1662b0b5597d99477d6ceabdc3d9
jiangzhonglian <jiang-s@163.com>
2018-04-03 00:55:54 +0800

    更新注释

--- src/py3.x/PyTorch/CNN-DigitRecognizer.py
+++ src/py3.x/PyTorch/CNN-DigitRecognizer.py
@@ -48,20 +48,24 @@ class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
         self.conv1 = nn.Sequential(  # input shape (1, 28, 28)
+            # output shape (16, 28, 28)  28=(width+2*padding-kernel_size)/stride+1
             nn.Conv2d(
-                in_channels=1,  # input height
-                out_channels=16,  # n_filters
-                kernel_size=5,  # filter size
-                stride=1,  # filter movement/step
-                padding=2,  # if want same width and length of this image after con2d, padding=(kernel_size-1)/2 if stride=1
-            ),  # output shape (16, 28, 28)
+                in_channels=1,    # 输入信号的通道数
+                out_channels=16,  # 卷积后输出结果的通道数
+                kernel_size=5,    # 卷积核的形状
+                stride=1,         # 卷积核得步长
+                padding=2,        # 处理边界时在每个维度首尾补0数量 padding=(kernel_size-1)/2 if stride=1
+            ),
             nn.ReLU(),  # activation
-            nn.MaxPool2d(kernel_size=2),  # choose max value in 2x2 area, output shape (16, 14, 14)
+            # output shape (16, 14, 14)  14=(width+0*padding-dilation*(kernel_size-1)-1)/stride+1
+            # dilation=1 默认值为0
+            nn.MaxPool2d(kernel_size=2, stride=2),  # 最大池化操作时的窗口大小
         )
-        self.conv2 = nn.Sequential(  # input shape (1, 14, 14)
+        # output shape (32, 7, 7)
+        self.conv2 = nn.Sequential(      # input shape (1, 14, 14)
             nn.Conv2d(16, 32, 5, 1, 2),  # output shape (32, 14, 14)
             nn.ReLU(),  # activation
-            nn.MaxPool2d(2),  # output shape (32, 7, 7)
+            nn.MaxPool2d(2, 2),
         )
         self.out = nn.Linear(32 * 7 * 7, 10)  # fully connected layer, output 10 classes
 
