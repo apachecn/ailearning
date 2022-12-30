a270e5f1038df762d61301d18dbf062cbde1c2cd
片刻小哥哥 <jiang-s@163.com>
2022-12-30 16:41:31 +0800

    修复删除的内容

---
+++ run_example.py
@@ -0,0 +1,20 @@
+# import tutorials.keras.text_NER as ft
+# import tutorials.keras.brat_tag as ft
+import tutorials.RecommenderSystems.rs_rating_demo as ft
+# from middleware.utils import TimeStat, Chart
+# import matplotlib.pyplot as plt
+# import matplotlib.gridspec as gridspec
+# from matplotlib.font_manager import FontProperties
+# plt.rcParams['font.sans-serif'] = ['SimHei']
+# plt.rcParams['axes.unicode_minus'] = False
+
+
+def main():
+    ft.main()
+    # x=y=[1,2,3]
+    # plt.plot(x, y, color='g', linestyle='-')  # 绘制
+    # plt.grid(True, ls = '--')
+    # plt.show()
+
+if __name__ == "__main__":
+    main()
