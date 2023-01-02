9c5ba2b96cb59e79ff0d33a2f1a59f9b4a2dc712
片刻小哥哥 <jiang-s@163.com>
2023-01-02 22:54:18 +0800

    添加QQ群

--- asset/docsify-apachecn-footer.js
+++ asset/docsify-apachecn-footer.js
@@ -7,7 +7,7 @@
         <div align="center">
           <p><a href="http://www.apachecn.org/" target="_blank"><font face="KaiTi" size="6" color="red">我们一直在努力</font></a><p>
           <p><a href="https://github.com/${unRepo}" target="_blank">${unRepo}</a></p>
-          <p><a target="_blank" href="https://jq.qq.com/?_wv=1027&k=fgYM7eMw"><img border="0" src="//pub.idqqimg.com/wpa/images/group.png" alt="iBooker 面试求职" title="iBooker 面试求职"></a></p>
+          <p><a target="_blank" href="https://qm.qq.com/cgi-bin/qm/qr?k=2oFEp1KHbDCP0Te4Wt-I6FOK4hvg4iBk&jump_from=webapi&authKey=dw08LmD1w9km55TSmcW2J4gjeaiyn7KTff+8bnqiIeDweqrzQF2ccsE/hQswWQk7"><img border="0" src="//pub.idqqimg.com/wpa/images/group.png" alt="iBooker 面试求职" title="iBooker 面试求职"></a></p>
           <p><span id="cnzz_stat_icon_${cnzzId}"></span></p>
           <div class="wwads-cn wwads-horizontal" data-id="206" style="max-width:350px"></div>
           <div style="text-align:center;margin:0 0 10.5px;">
