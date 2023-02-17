b404758ba57bef414fa38549d37f7bed9cbc185d
片刻小哥哥 <jiang-s@163.com>
2023-02-18 01:25:56 +0800

    添加评论系统

--- asset/docsify-apachecn-footer.js
+++ asset/docsify-apachecn-footer.js
@@ -2,7 +2,7 @@
 	var cnzzId = window.$docsify.cnzzId
 	var unRepo = window.$docsify.repo || ''
 	var [un, repo] = unRepo.split('/')
-    var footer = `
+  var footer = `
       <hr/>
       <div align="center">
         <p><a href="http://www.apachecn.org/" target="_blank"><font face="KaiTi" size="6" color="red">我们一直在努力</font></a><p>
@@ -12,7 +12,24 @@
         <p><a href="https://get.brightdata.com/apachecn" target="_blank"><img src="img/ad/partnerstack.gif" /></a><p>
       </div>
       <hr/>
-      <div id="gitalk-container" ></div>
+      <!-- 来必力City版安装代码 -->
+      <div id="lv-container" data-id="city" data-uid="MTAyMC81ODA2NC8zNDUyNw==">
+        <script type="text/javascript">
+        (function(d, s) {
+            var j, e = d.getElementsByTagName(s)[0];
+
+            if (typeof LivereTower === 'function') { return; }
+
+            j = d.createElement(s);
+            j.src = 'https://cdn-city.livere.com/js/embed.dist.js';
+            j.async = true;
+
+            e.parentNode.insertBefore(j, e);
+        })(document, 'script');
+        </script>
+      <noscript> 为正常使用来必力评论功能请激活JavaScript</noscript>
+      </div>
+      <!-- City版安装代码已完成 -->
 	`
   var plugin = function(hook) {
     hook.afterEach(function(html) {
@@ -20,8 +37,6 @@
     })
     hook.doneEach(function() {
       (adsbygoogle = window.adsbygoogle || []).push({})
-      new Gitalk(window.$docsify.gitalk)
-          .render(window.$docsify.gitalk.container)
     })
   }
   var plugins = window.$docsify.plugins || []
